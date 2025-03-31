8f5b9786fd707275f40190e809151effd8f6a6f4
YutoNishigaki <nabe.n12@hotmail.co.jp>
2025-03-31 22:07:23 +0900

    nestjsを削除

--- backend/src/orm/orm.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrmService } from './orm.service';
-
-@Module({
-  providers: [OrmService],
-  exports: [OrmService],
-})
-export class OrmModule {}
