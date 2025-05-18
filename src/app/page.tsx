f712fd93289dc5f9e4daef642f997c870b771ac3
YutoNishigaki <nabe.n12@hotmail.co.jp>
2025-05-18 21:20:26 +0900

    shadcn/uiの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Form from "next/form";
 import { PrismaClient } from "@prisma/client";
+import { Button } from "@/components/ui/button";
 
 const prisma = new PrismaClient();
 
@@ -57,6 +58,7 @@ export default function Home() {
             className="text-black mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
+        <Button>Click me</Button>
         <button
           type="submit"
           className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
