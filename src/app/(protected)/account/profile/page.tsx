d99f1d816b7ec76d247889c0b2c080e604123eec
YutoNishigaki <nabe.n12@hotmail.co.jp>
2025-05-29 21:51:20 +0900

    セッションユーザーは取得する影響で認証済みページはSSRに修正

--- src/app/(protected)/account/profile/page.tsx
+++ src/app/(protected)/account/profile/page.tsx
@@ -1,5 +1,7 @@
 import { ProfileFormContainer } from "@/features/user/profile-form";
 
+export const dynamic = "force-dynamic";
+
 export default function Profile() {
   return <ProfileFormContainer />;
 }
