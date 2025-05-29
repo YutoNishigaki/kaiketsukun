d3b2e3c037e755dad0f1e68f6a4fa78bf6b38f58
YutoNishigaki <nabe.n12@hotmail.co.jp>
2025-05-29 21:04:03 +0900

    サーバー側でセッションを参照しているためSSRへ変更

--- src/app/(protected)/account/profile/page.tsx
+++ src/app/(protected)/account/profile/page.tsx
@@ -1,5 +1,7 @@
 import { ProfileForm } from "@/features/user/profile-form.presentational";
 
+export const dynamic = "force-dynamic";
+
 export default async function Dashboard() {
   return <ProfileForm />;
 }
