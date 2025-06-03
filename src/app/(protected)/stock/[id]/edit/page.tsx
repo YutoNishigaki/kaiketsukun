49005c209143d0a8d36f53fd793a01614169f618
YutoNishigaki <nabe.n12@hotmail.co.jp>
2025-06-03 22:08:58 +0900

    PageParamsの取得は非同期にする必要があったため修正

--- src/app/(protected)/stock/[id]/edit/page.tsx
+++ src/app/(protected)/stock/[id]/edit/page.tsx
@@ -2,14 +2,16 @@ export const dynamic = "force-dynamic";
 
 import { StockEditContainer } from "@/features/stock/edit";
 
-type Props = {
-  params: {
-    id: string;
-  };
+type Params = {
+  id: string;
 };
 
-export default function EditStockPage(props: Props) {
-  const { id } = props.params;
+export default async function EditStockPage({
+  params,
+}: {
+  params: Promise<Params>;
+}) {
+  const { id } = await params;
 
   return <StockEditContainer id={id} />;
 }
